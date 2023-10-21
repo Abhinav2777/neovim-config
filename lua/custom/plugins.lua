441cd769e7a706e69e9ec5647d8cc8a23ade05f4
Abhinav Sai D <abhinavsai491@tutanota.com>
2023-10-21 15:58:08 +0530

    Configuration and customization of the neovim text editor

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,121 @@
+local overrides = require("custom.configs.overrides")
+local plugins = {
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      require("dapui").setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function ()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function ()
+      require "custom.configs.dap"
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "mhartington/formatter.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require "custom.configs.formatter"
+    end
+  },
+  { "elkowar/yuck.vim" , lazy = false }, -- load a plugin at startup
+  --{
+   -- "mfussenegger/nvim-lint",
+  --  event = "VeryLazy",
+  --  config = function ()
+  --    require "custom.configs.lint"
+  --  end
+  --},
+  { "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  -- You can use any plugin specification from lazy.nvim
+  {
+    "Pocco81/TrueZen.nvim",
+    cmd = { "TZAtaraxis", "TZMinimalist" },
+    config = function()
+      require "custom.configs.truezen" -- just an example path
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+  -- In order to modify the `lspconfig` configuration:
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+   end,
+  },
+  {
+    "folke/which-key.nvim",
+    enabled = true,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason
+  },
+
+  -- If your opts uses a function call, then make opts spec a function*
+  -- should return the modified default config as well
+  -- here we just call the default telescope config 
+  -- and then assign a function to some of its options
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function()
+      local conf = require "plugins.configs.telescope"
+      conf.defaults.mappings.i = {
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<Esc>"] = require("telescope.actions").close,
+      }
+
+      return conf
+    end,
+  }
+}
+
+return plugins
+
